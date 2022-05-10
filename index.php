818253185feb697b182417a68dcded456d6fc574
kotuvijay <104496284+kotuvijay@users.noreply.github.com>
2022-05-10 16:30:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<body style="background-color:green;">
+<body style="background-color:lightblue;">
 <font size = "6"><p style="color:red">This is Prodution deployment Slot.</p></font>
 
 <?php
