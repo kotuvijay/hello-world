b3155e7e72d44c02ffa78ca401d6ff5ded2315d4
kotuvijay <104496284+kotuvijay@users.noreply.github.com>
2022-05-10 16:25:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <body>
 
-<body style="background-color:powderblue;">
-<font size = "6"><p style="color:red">This is Stagging deployment Slot.</p></font>
+<body style="background-color:green;">
+<font size = "6"><p style="color:red">This is Prodution deployment Slot.</p></font>
 
 <?php
 echo "Azure App Service Example";
